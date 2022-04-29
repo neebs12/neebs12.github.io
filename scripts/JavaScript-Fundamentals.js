f9a8e8e91b34cf7041632967239a78fca99462c8
Jason Aricheta <jason.aricheta@gmail.com>
2022-04-30 06:55:56 +1200

    second feature completed

--- scripts/JavaScript-Fundamentals.js
+++ scripts/JavaScript-Fundamentals.js
@@ -7,19 +7,46 @@ let img;
 
 function firstExampleHandler(event) {
   // not yet created
-  if (img !== firstExample.lastElementChild) {
+  if (firstExample.lastElementChild.tagName !== "IMG") {
     img = document.createElement('img');  
     firstExample.insertAdjacentElement("beforeend", img);  
     img.style.width = 'auto';
+    img.classList.add("daniel");
   } 
-  img.setAttribute('src', event.target.value);
+  // should update *all* daniel imgs as this part is toggled
+  // -- applicable to any examples
+  [].forEach.call(document.getElementsByClassName('daniel'), 
+    img => img.setAttribute('src', event.target.value)
+  );
 }
 
 // work on second example
-function secondExampleHandler() {}
+function secondExampleHandler(event) {
+  if (!img) {
+    alert("Please select a Daniel before toggling for this example!")
+    return;
+  }
+
+  // first, remove all children of secondExample except for the fieldset
+  let tmp = secondExample.lastElementChild;
+  while (tmp.tagName !== 'FIELDSET') {
+    tmp.remove();
+    tmp = secondExample.lastElementChild;
+  }
+
+  let val = event.target.value;
+  // img is the current element
+  // we clone it, 
+  // at this part of the code, img is a valid HTMLIMGElement object
+  for (let i = 0; i < val; i += 1) {
+    let copyImg = img.cloneNode(false);
+    secondExample.insertAdjacentElement("beforeend", copyImg);
+  }
+}
 
 function registerHandlers() {
-  firstExample.addEventListener('change', firstExampleHandler)
+  firstExample.addEventListener('change', firstExampleHandler);
+  secondExample.addEventListener('change', secondExampleHandler);
 }
 
 document.addEventListener('DOMContentLoaded', event => {

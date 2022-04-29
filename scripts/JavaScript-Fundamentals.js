5035b8ecceca74764a78f6480bda9082a98e1995
Jason Aricheta <jason.aricheta@gmail.com>
2022-04-29 20:56:18 +1200

    added first example handler

--- scripts/JavaScript-Fundamentals.js
+++ scripts/JavaScript-Fundamentals.js
@@ -1,1 +1,30 @@
 console.log('Hello World');
+
+let firstExample;
+let secondExample; 
+let thirdExample;
+let img;
+
+function firstExampleHandler(event) {
+  // not yet created
+  if (img !== firstExample.lastElementChild) {
+    img = document.createElement('img');  
+    firstExample.insertAdjacentElement("beforeend", img);  
+    img.style.width = 'auto';
+  } 
+  img.setAttribute('src', event.target.value);
+}
+
+// work on second example
+function secondExampleHandler() {}
+
+function registerHandlers() {
+  firstExample.addEventListener('change', firstExampleHandler)
+}
+
+document.addEventListener('DOMContentLoaded', event => {
+  firstExample = document.getElementById('first-example');
+  secondExample = document.getElementById('second-example');
+  thirdExample = document.getElementById('third-example');
+  registerHandlers();
+});
